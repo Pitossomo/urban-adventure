b26ef670a210c8856ad1e202890de0d9df22527c
pitossomo <pedro.h.teles@hotmail.com>
2022-07-30 09:34:04 -0300

    removed database query in the bearer auth middleware since, once the token is validated, it provides the info we need

--- src/middlewares/bearerAuthentication.middleware.ts
+++ src/middlewares/bearerAuthentication.middleware.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import ForbiddenError from "../models/errors/forbidden.error.model";
-import userRepository from "../repositories/user.repository";
 
-async function bearerAuthenticationMiddleWare(req: Request, res: Response, next: NextFunction ): User{
+function bearerAuthenticationMiddleWare(req: Request, res: Response, next: NextFunction ): User{
   try {
     const authHeader = req.get('authorization')
     if (!authHeader) throw new ForbiddenError('Credenciais não informadas')
@@ -14,9 +13,10 @@ async function bearerAuthenticationMiddleWare(req: Request, res: Response, next:
     const tokenPayload = jwt.verify(token, process.env.JWT_SECRET_KEY as string)
     
     if (typeof tokenPayload !== 'object' || !tokenPayload.sub) throw new ForbiddenError('Token inválido')
-    const uuid = tokenPayload.sub
-
-    const user = await userRepository.findById(uuid)
+    const user = {
+      uuid: tokenPayload.sub,
+      username: tokenPayload.username
+    }
 
     req.user = user
     next()
